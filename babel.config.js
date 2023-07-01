088216b38f43dc1388590c4c04dab5354dcec13c
namvoathena <namvp@athena.studio>
2023-07-01 08:23:37 +0700

    first commit

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            components: "./components",
+            screens: "./screens",
+            assets: "./assets",
+            styles: "./styles",
+            firebaseServices: "./firebaseServices",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
+  };
+};
